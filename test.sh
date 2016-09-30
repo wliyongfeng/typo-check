5572a9a14acf75b9b8da51ef75781905bfcb7648
Yongfeng LI <wliyongfeng@gmail.com>
2016-09-30 11:14:22 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+export PATH=/Users/leo/.pyenv/shims:/Users/leo/.pyenv/shims:/Users/leo/myscripts:/Users/leo/Library/Android/sdk/platform-tools:/Users/leo/Library/Android/sdk/tools:/Users/leo/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/Cellar/node/4.2.1/bin:/usr/local/mysql/bin
+
+echo $PATH
